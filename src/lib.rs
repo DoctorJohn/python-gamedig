ed05e39cdd9f880113a93624620106f2d710835f
Jonathan Ehwald <github@ehwald.info>
2024-10-04 18:56:02 +0200

    Add a dedicated error for each upstream error kind

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-use ::gamedig as rust_gamedig;
-use pyo3::{
-    create_exception,
-    exceptions::{PyException, PyValueError},
-    prelude::*,
-};
-use serde_pyobject::to_pyobject;
+mod errors;
 
-create_exception!(gamedig, GameDigError, PyException);
+use ::gamedig as rust_gamedig;
+use errors::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
+use serde_pyobject::to_pyobject;
 
 #[pyfunction]
 #[pyo3(signature = (game_id, address, port=None))]
@@ -22,7 +19,7 @@ fn query(py: Python, game_id: &str, address: &str, port: Option<u16>) -> PyResul
     };
 
     match rust_gamedig::query(game, &parsed_address, port) {
-        Err(err) => return Err(GameDigError::new_err(format!("{:?}", err.kind))),
+        Err(err) => return Err(gd_error_to_py_err(err)),
         Ok(response) => {
             let response_json = response.as_json();
             let py_response = to_pyobject(py, &response_json).unwrap();
@@ -34,6 +31,55 @@ fn query(py: Python, game_id: &str, address: &str, port: Option<u16>) -> PyResul
 #[pymodule]
 fn gamedig(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("GameDigError", m.py().get_type_bound::<GameDigError>())?;
+    m.add(
+        "PacketOverflowError",
+        m.py().get_type_bound::<PacketOverflowError>(),
+    )?;
+    m.add(
+        "PacketUnderflowError",
+        m.py().get_type_bound::<PacketUnderflowError>(),
+    )?;
+    m.add("PacketBadError", m.py().get_type_bound::<PacketBadError>())?;
+    m.add(
+        "PacketSendError",
+        m.py().get_type_bound::<PacketSendError>(),
+    )?;
+    m.add(
+        "PacketReceiveError",
+        m.py().get_type_bound::<PacketReceiveError>(),
+    )?;
+    m.add(
+        "DigDecompressError",
+        m.py().get_type_bound::<DigDecompressError>(),
+    )?;
+    m.add(
+        "DigSocketConnectError",
+        m.py().get_type_bound::<DigSocketConnectError>(),
+    )?;
+    m.add(
+        "SocketBindError",
+        m.py().get_type_bound::<SocketBindError>(),
+    )?;
+    m.add(
+        "InvalidInputError",
+        m.py().get_type_bound::<InvalidInputError>(),
+    )?;
+    m.add("BadGameError", m.py().get_type_bound::<BadGameError>())?;
+    m.add("AutoQueryError", m.py().get_type_bound::<AutoQueryError>())?;
+    m.add(
+        "ProtocolFormatError",
+        m.py().get_type_bound::<ProtocolFormatError>(),
+    )?;
+    m.add(
+        "UnknownEnumCastError",
+        m.py().get_type_bound::<UnknownEnumCastError>(),
+    )?;
+    m.add("JsonParseError", m.py().get_type_bound::<JsonParseError>())?;
+    m.add("TypeParseError", m.py().get_type_bound::<TypeParseError>())?;
+    m.add(
+        "HostLookupError",
+        m.py().get_type_bound::<HostLookupError>(),
+    )?;
     m.add_function(wrap_pyfunction!(query, m)?)?;
     Ok(())
 }
