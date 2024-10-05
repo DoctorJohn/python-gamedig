73e5c3d33fe2094311227f61760a8c20176247b5
Jonathan Ehwald <github@ehwald.info>
2024-10-05 20:22:44 +0200

    Support timeout settings (#6)

--- tests/test_gamedig.py
+++ tests/test_gamedig.py
@@ -14,6 +14,35 @@ def test_query_raises_for_invalid_addresses(invalid_address):
         gamedig.query("minecraft", invalid_address)
 
 
+def test_query_connect_timeout_duration_must_not_be_0():
+    with pytest.raises(
+        gamedig.InvalidInputError, match="Connect duration must not be 0"
+    ):
+        gamedig.query(
+            "minecraft",
+            "127.0.0.1",
+            timeout_settings={"retries": 0, "connect": 0},
+        )
+
+
+def test_query_read_timeout_duration_must_not_be_0():
+    with pytest.raises(gamedig.InvalidInputError, match="Read duration must not be 0"):
+        gamedig.query(
+            "minecraft",
+            "127.0.0.1",
+            timeout_settings={"retries": 0, "read": 0},
+        )
+
+
+def test_query_write_timeout_duration_must_not_be_0():
+    with pytest.raises(gamedig.InvalidInputError, match="Write duration must not be 0"):
+        gamedig.query(
+            "minecraft",
+            "127.0.0.1",
+            timeout_settings={"retries": 0, "write": 0},
+        )
+
+
 def test_query_raises_for_auto_query_errors():
     with pytest.raises(gamedig.AutoQueryError):
         gamedig.query("minecraft", "127.0.0.1")
