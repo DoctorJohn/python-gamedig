a480c2851285a2840d664832575c9d5d15312652
Jonathan Ehwald <github@ehwald.info>
2025-05-02 21:25:27 +0200

    Upgrade to pyo3 0.24

--- src/lib.rs
+++ src/lib.rs
@@ -6,56 +6,44 @@ use pyo3::prelude::*;
 
 #[pymodule]
 fn gamedig(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add("GameDigError", m.py().get_type_bound::<GameDigError>())?;
+    m.add("GameDigError", m.py().get_type::<GameDigError>())?;
     m.add(
         "PacketOverflowError",
-        m.py().get_type_bound::<PacketOverflowError>(),
+        m.py().get_type::<PacketOverflowError>(),
     )?;
     m.add(
         "PacketUnderflowError",
-        m.py().get_type_bound::<PacketUnderflowError>(),
+        m.py().get_type::<PacketUnderflowError>(),
     )?;
-    m.add("PacketBadError", m.py().get_type_bound::<PacketBadError>())?;
-    m.add(
-        "PacketSendError",
-        m.py().get_type_bound::<PacketSendError>(),
-    )?;
+    m.add("PacketBadError", m.py().get_type::<PacketBadError>())?;
+    m.add("PacketSendError", m.py().get_type::<PacketSendError>())?;
     m.add(
         "PacketReceiveError",
-        m.py().get_type_bound::<PacketReceiveError>(),
+        m.py().get_type::<PacketReceiveError>(),
     )?;
     m.add(
         "DigDecompressError",
-        m.py().get_type_bound::<DigDecompressError>(),
+        m.py().get_type::<DigDecompressError>(),
     )?;
     m.add(
         "DigSocketConnectError",
-        m.py().get_type_bound::<DigSocketConnectError>(),
+        m.py().get_type::<DigSocketConnectError>(),
     )?;
-    m.add(
-        "SocketBindError",
-        m.py().get_type_bound::<SocketBindError>(),
-    )?;
-    m.add(
-        "InvalidInputError",
-        m.py().get_type_bound::<InvalidInputError>(),
-    )?;
-    m.add("BadGameError", m.py().get_type_bound::<BadGameError>())?;
-    m.add("AutoQueryError", m.py().get_type_bound::<AutoQueryError>())?;
+    m.add("SocketBindError", m.py().get_type::<SocketBindError>())?;
+    m.add("InvalidInputError", m.py().get_type::<InvalidInputError>())?;
+    m.add("BadGameError", m.py().get_type::<BadGameError>())?;
+    m.add("AutoQueryError", m.py().get_type::<AutoQueryError>())?;
     m.add(
         "ProtocolFormatError",
-        m.py().get_type_bound::<ProtocolFormatError>(),
+        m.py().get_type::<ProtocolFormatError>(),
     )?;
     m.add(
         "UnknownEnumCastError",
-        m.py().get_type_bound::<UnknownEnumCastError>(),
+        m.py().get_type::<UnknownEnumCastError>(),
     )?;
-    m.add("JsonParseError", m.py().get_type_bound::<JsonParseError>())?;
-    m.add("TypeParseError", m.py().get_type_bound::<TypeParseError>())?;
-    m.add(
-        "HostLookupError",
-        m.py().get_type_bound::<HostLookupError>(),
-    )?;
+    m.add("JsonParseError", m.py().get_type::<JsonParseError>())?;
+    m.add("TypeParseError", m.py().get_type::<TypeParseError>())?;
+    m.add("HostLookupError", m.py().get_type::<HostLookupError>())?;
     m.add_function(wrap_pyfunction!(crate::query::query, m)?)?;
     Ok(())
 }
